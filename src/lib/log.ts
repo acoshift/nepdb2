343eba757875b10fd24842fa09b462eea183738a
Thanatat Tamtan <acoshift@gmail.com>
2016-01-23 19:12:57 +0700

    update

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,5 +1,16 @@
 import { Request } from '../nepdb.d';
+import { collection } from '../utils';
 
 export = function(r: Request): void {
-  // TODO: write log
+  let l = {
+    ns: r.ns,
+    user: r.user._id,
+    role: r.user.role,
+    nq: {
+      method: r.nq.method,
+      name: r.nq.name
+    },
+    status: r.status
+  };
+  collection(r, 'db.logs').insertOne(l, { w: 0 }, null);
 }
