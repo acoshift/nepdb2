b64a1511422ecb59ece4d82a7dead9036a3e5f47
Thanatat Tamtan <acoshift@gmail.com>
2016-01-26 03:07:38 +0700

    support: both user id and user name or owner access

--- src/lib/op/list.ts
+++ src/lib/op/list.ts
@@ -7,7 +7,7 @@ import httpStatus = require('http-status');
 export = function(r: Request): Observable<Request> {
   let access = canAccess(r, 'r');
   if (access === 0) return Observable.throw(reject(r, httpStatus.UNAUTHORIZED));
-  if (access === 2 && !r.user._id) return Observable.throw(reject(r, httpStatus.UNAUTHORIZED));
+  if (access === 2 && !(r.user._id || r.user.name)) return Observable.throw(reject(r, httpStatus.UNAUTHORIZED));
 
   let nq = r.nq;
 
